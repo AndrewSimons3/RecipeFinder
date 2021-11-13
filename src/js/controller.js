3ebb5189d47b1f37e132b09d64fcbc046f22abbd
AndrewSimons3 <simdrew07@gmail.com>
2021-11-13 10:30:07 -0600

    implemented retreiving search results loading and rendering them

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,15 +1,14 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js'
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-// https://forkify-api.herokuapp.com/v2
 
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
-    console.log(id)
 
     if (!id) return;
     recipeView.renderSpinner();
@@ -24,7 +23,25 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function() {
+  try {
+    // Get Search Query
+    const query = searchView.getQuery();
+    if(!query) return;
+
+    // Load Search Results
+    await model.loadSearchResults(query);
+
+    // Render Results
+    console.log(model.state.search.results);
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+
 const init = function() {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 }
 init();

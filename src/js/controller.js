7b1954f4b3ba7238526bb4a10245ddec4ad06163
AndrewSimons3 <simdrew07@gmail.com>
2021-11-13 09:33:55 -0600

    first

---
+++ src/js/controller.js
@@ -0,0 +1,30 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+// https://forkify-api.herokuapp.com/v2
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    console.log(id)
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // Loading recipe
+    await model.loadRecipe(id);
+
+    // Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+const init = function() {
+  recipeView.addHandlerRender(controlRecipes);
+}
+init();

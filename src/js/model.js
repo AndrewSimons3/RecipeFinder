218c577f64d99ed7f159a75e0114738147bd7855
AndrewSimons3 <simdrew07@gmail.com>
2021-11-14 09:30:13 -0600

    implemented getSearchResultsPage function to render 10 search results per page

--- src/js/model.js
+++ src/js/model.js
@@ -1,5 +1,5 @@
 import { async } from 'regenerator-runtime';
-import { API_URL } from './config';
+import { API_URL, RES_PER_PAGE } from './config';
 import { getJSON } from './helpers';
 
 export const state = {
@@ -7,6 +7,8 @@ export const state = {
   search: {
     query: '',
     results: [],
+    page: 1,
+    resultsPerPage: RES_PER_PAGE,
   }
 };
 
@@ -47,10 +49,18 @@ export const loadSearchResults = async function (query) {
         image: rec.image_url,
       };
     });
-    console.log(state.search.results)
   } catch (err) {
     console.error(`${err} 💥`);
     throw err;
   }
 };
+
+export const getSearchResultsPage = function(page = state.search.page) {
+  state.search.page = page;
+
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = page * state.search.resultsPerPage;
+
+  return state.search.results.slice(start, end);
+}
 

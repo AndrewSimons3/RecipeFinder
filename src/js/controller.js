ac35c80762899068a89efc67f026a4a56e9a2afb
AndrewSimons3 <simdrew07@gmail.com>
2021-11-14 08:41:11 -0600

    added View and resultsView class

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,6 +1,7 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
-import searchView from './views/searchView.js'
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -25,6 +26,9 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function() {
   try {
+    resultsView.renderSpinner();
+    console.log(resultsView);
+
     // Get Search Query
     const query = searchView.getQuery();
     if(!query) return;
@@ -34,6 +38,7 @@ const controlSearchResults = async function() {
 
     // Render Results
     console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch(err) {
     console.log(err);
   }
